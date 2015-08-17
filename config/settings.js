7547bc69fb0473d9a5b5b6377e739e00a85043be
tony.pizzicato <tony.grebnev@gmail.com>
2015-08-18 00:04:41 +0300

    fixed contact email

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     showPromoTournamentLink: false,
-    ordersEmail: 'umerov.ervin2013@yandex.ru',
-    //ordersEmail: 'tony.grebnev@gmail.com',
+    //ordersEmail: 'umerov.ervin2013@yandex.ru',
+    ordersEmail: 'tony.grebnev@gmail.com',
     ordersBcc: 'tony.grebnev@gmail.com'
 };
