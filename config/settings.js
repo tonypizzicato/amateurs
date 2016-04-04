4fb94e3d51d89321b546d349459b57a1f8538a10
tony.pizzicato <tony.grebnev@gmail.com>
2016-04-04 13:54:11 +0300

    config file syntax fix

--- config/settings.js
+++ config/settings.js
@@ -2,6 +2,6 @@ module.exports = {
     showPromoTournamentLink: false,
     ordersEmail: 'umerov.ervin2013@yandex.ru',
     //ordersEmail: 'tony.grebnev@gmail.com',
-    ordersBcc: 'tony.grebnev@gmail.com'
+    ordersBcc: 'tony.grebnev@gmail.com',
     api: 'http://sharvester.ru/api'
 };
