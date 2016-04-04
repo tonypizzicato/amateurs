7aeb394659dde85951353683f613545cae58b7a6
tony.pizzicato <tony.grebnev@gmail.com>
2016-04-04 13:44:52 +0300

    minor fixes

--- config/settings.js
+++ config/settings.js
@@ -3,4 +3,5 @@ module.exports = {
     //ordersEmail: 'umerov.ervin2013@yandex.ru',
     ordersEmail: 'tony.grebnev@gmail.com',
     ordersBcc: 'tony.grebnev@gmail.com'
+    api: 'http://sharvester.ru/api'
 };
