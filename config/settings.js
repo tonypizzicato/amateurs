bd612a15f4cf4fb197b773625629b50d37da9c06
tony.pizzicato <tony.grebnev@gmail.com>
2016-05-21 02:12:50 +0300

    new partners; orders emails

--- config/settings.js
+++ config/settings.js
@@ -4,5 +4,13 @@ module.exports = {
     //ordersEmail: 'umerov.ervin2013@yandex.ru',
     ordersEmail: 'tony.grebnev@gmail.com',
     ordersBcc: 'tony.grebnev@gmail.com',
+    // ordersMoscow: 'umerov.ervin2013@yandex.ru',
+    // ordersSpb: 's.skurikhin@gmail.com',
+    // ordersRostov: 'ivanevronimus@gmail.com',
+    // ordersEkb: 'slabyi_27@mail.ru',
+    // ordersKazan: 'umerov.ervin2013@yandex.ru',
+    // "ordersY-ola": 'umerov.ervin2013@yandex.ru',
+    // ordersAnapa: 'umerov.ervin2013@yandex.ru',
+    // ordersIzhevsk: 'umerov.ervin2013@yandex.ru',
     api: 'http://sharvester.ru/api'
 };
