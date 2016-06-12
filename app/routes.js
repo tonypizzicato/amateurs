7f5646011228b69952aee152b173df6b9d19728d
tony.pizzicato <tony.grebnev@gmail.com>
2016-06-13 01:59:41 +0200

    beach soccer

--- app/routes.js
+++ app/routes.js
@@ -162,7 +162,7 @@ module.exports.initialize = function (app) {
     app.get('/:league/tournaments/:name/fields/:fieldName', t(''), news.pre, tournaments.globals, fields.item);
     app.get('/:league/tournaments/:name', t('Чемпионат'), news.pre, tournaments.globals, tournaments.item);
 
-    app.get('/:league(moscow|spb|ekb|kazan|y-ola|rostov|anapa|izhevsk|kiev)', t('Турниры'), news.pre, leagues.item);
+    app.get('/:league(moscow|spb|ekb|kazan|y-ola|rostov|anapa|izhevsk|kiev|beach)', t('Турниры'), news.pre, leagues.item);
 
     app.get('/:league/players/:id', t(''), news.pre, news.globals, players.item);
     app.get('/:league/contacts', t('Контакты'), news.pre, news.globals, contacts.list);
