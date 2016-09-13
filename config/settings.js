747468ff19453c304ffee3827bdf09c7c471c0a1
tony.pizzicato <tony.grebnev@gmail.com>
2016-09-14 01:55:22 +0300

    new build

--- config/settings.js
+++ config/settings.js
@@ -1,11 +1,11 @@
 module.exports = {
     showPromoTournamentLink: false,
     showKiev: true,
-    ordersEmail: 'eumerov.afl@gmail.com',
+    ordersEmail: 'ngordeev.afl@gmail.com, eumerov.afl@gmail.com',
     ordersEmailPlayer: 'lozovoy@s-inn.ru',
     // ordersEmail: 'tony.grebnev@gmail.com',
     ordersBcc: 'tony.grebnev@gmail.com',
-    ordersMoscow: 'eumerov.afl@gmail.com',
+    ordersMoscow: 'ngordeev.afl@gmail.com, eumerov.afl@gmail.com',
     ordersSpb: 's.skurikhin@gmail.com',
     ordersRostov: 'ivanevronimus@gmail.com',
     ordersEkb: 'slabyi_27@mail.ru',
