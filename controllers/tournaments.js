dccc75484a9def9bf82344913e2fb984c6af6c0b
tony.pizzicato <tony.grebnev@gmail.com>
2016-09-14 01:54:06 +0300

    fix photos order

--- controllers/tournaments.js
+++ controllers/tournaments.js
@@ -397,7 +397,7 @@ module.exports = {
                             .then(response => {
                                 const res = [];
 
-                                const games = _.sortBy(response.data, 'timestamp');
+                                const games = _.orderBy(response.data, 'timestamp', 'desc');
 
                                 games.forEach(function (item) {
                                     res.push({
